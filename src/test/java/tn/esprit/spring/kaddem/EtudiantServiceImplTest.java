97e29e95b71355fba9a6af7a56dc83067fd78e26
balkis melki <melki.balkis@esprit.tn>
2023-10-24 21:52:13 +0100

    updated test

--- src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImplTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.entities.Option;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.services.IEtudiantService;
 
@@ -19,9 +20,20 @@ class EtudiantServiceImplTest {
 
     @Test
     void testAddEtudiant() {
+        // Create an "Etudiant" object with specific attributes
         Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1); // Set the ID to 1
+        etudiant.setNomE("Balkis"); // Set the name to "Balkis"
+        etudiant.setPrenomE("Melki"); // Set the prenom
+        etudiant.setOp(Option.GAMIX); // Set the option (replace "YourOptionHere" with the actual option)
+
         Etudiant result = etudiantService.addEtudiant(etudiant);
+
+        // Check if the entity was successfully added
         assertNotNull(result.getIdEtudiant());
+        assertEquals("Balkis", result.getNomE());
+        assertEquals("Melki", result.getPrenomE());
+        assertEquals(Option.GAMIX, result.getOp());
     }
 
 
