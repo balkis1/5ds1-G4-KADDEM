e183cdc47a65140c475639573f897f39a795f69c
balkis1 <92169272+balkis1@users.noreply.github.com>
2023-10-28 22:00:50 +0100

    Update EtudiantServiceImplTest.java

--- src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImplTest.java
@@ -1,40 +1,36 @@
 package tn.esprit.spring.kaddem;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.entities.Option;
 import tn.esprit.spring.kaddem.services.IEtudiantService;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
-public class EtudiantServiceImplTest {
+class EtudiantServiceImplTest {
     @Autowired
     private IEtudiantService etudiantService;
 
     @Test
-    public void testAddEtudiant() {
+    void testAddEtudiant() {
         // Create an "Etudiant" object with specific attributes
         Etudiant etudiant = new Etudiant();
         etudiant.setIdEtudiant(1); // Set the ID to 1
-        etudiant.setNomE("Ahmed"); // Set the name to "Ahmed"
+        etudiant.setNomE("Balkis"); // Set the name to "Balkis"
         etudiant.setPrenomE("Melki"); // Set the prenom
-        etudiant.setOp(Option.NIDS); // Set the option
+        etudiant.setOp(Option.GAMIX); // Set the option (replace "YourOptionHere" with the actual option)
 
         Etudiant result = etudiantService.addEtudiant(etudiant);
 
         // Check if the entity was successfully added
         assertNotNull(result.getIdEtudiant());
-        assertEquals("Ahmed", result.getNomE());
+        assertEquals("Balkis", result.getNomE());
         assertEquals("Melki", result.getPrenomE());
-        assertEquals(Option.NIDS, result.getOp());
+        assertEquals(Option.GAMIX, result.getOp());
     }
 }
