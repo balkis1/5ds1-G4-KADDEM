286e265e25c10d47283b970aced4c290046c2899
ghada101 <ghada.aissa@esprit.tn>
2023-11-01 18:03:53 +0100

    fix bugg

--- src/test/java/tn/esprit/spring/kaddem/KaddemApplicationTest.java
+++ src/test/java/tn/esprit/spring/kaddem/KaddemApplicationTest.java
@@ -1,11 +1,13 @@
 package tn.esprit.spring.kaddem;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import lombok.extern.slf4j.Slf4j;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.kaddem.entities.Contrat;
 import tn.esprit.spring.kaddem.entities.Specialite;
@@ -15,37 +17,26 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-@Slf4j
-@Service
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = KaddemApplication.class)
-
 public class KaddemApplicationTest {
     @Autowired
     IContratService contratService;
-    @Test
-    public void testAddContat() throws ParseException {
 
+    @Test
+    public void testAddContrat() throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date dateDebutContrat = dateFormat.parse("1/09/2000");
         Date dateFinContrat = dateFormat.parse("30/09/2000");
 
-        Specialite specialite = Specialite.IA;  // Replace IA with the desired enum value
+        Specialite specialite = Specialite.IA;
 
-        // Replace "True" with true
         boolean archive = true;
 
         Contrat c = new Contrat(dateDebutContrat, dateFinContrat, specialite, archive, 15);
 
         Contrat contrat = contratService.addContrat(c);
-        log.info("contraaat "+contrat);
         assertNotNull(contrat.getIdContrat());
         assertTrue(contrat.getArchive());
-
-    } }
+    }
+}
