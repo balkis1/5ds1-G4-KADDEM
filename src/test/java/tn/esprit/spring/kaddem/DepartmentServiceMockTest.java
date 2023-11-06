090d0a55a24027b7a06ce1763bb43045ff4755a1
hanine4 <98483882+hanine4@users.noreply.github.com>
2023-11-06 02:44:07 +0100

    Update DepartmentServiceMockTest.java

--- src/test/java/tn/esprit/spring/kaddem/DepartmentServiceMockTest.java
+++ src/test/java/tn/esprit/spring/kaddem/DepartmentServiceMockTest.java
@@ -25,7 +25,6 @@ public class DepartmentServiceMockTest {
 
         // Define the behavior of the mocked departementService
         when(departementService.addDepartement(any(Departement.class))).thenReturn(department);
-        // Test the service that depends on the mocked etudiantService
         Departement result = departementService.addDepartement(department);
 
         // Verify that the mocked service was called with the correct arguments
@@ -36,9 +35,8 @@ public class DepartmentServiceMockTest {
     }
     @Test
     public void testRetrieveAllDepartments() {
-        // Define the behavior of the mocked etudiantService
+        =
         when(departementService.retrieveAllDepartements()).thenReturn(Arrays.asList(new Departement(), new Departement()));
-        // Test the service that depends on the mocked etudiantService
         List<Departement> result = departementService.retrieveAllDepartements();
 
         // Verify that the mocked service was called
@@ -53,23 +51,14 @@ public class DepartmentServiceMockTest {
 
         department.setIdDepart(123); // Set the department ID to 123
         department.setNomDepart("Computer Science");
-        // Define the behavior of the mocked etudiantService
         when(departementService.updateDepartement(any(Departement.class))).thenReturn(department);
-
-        // Test the service that depends on the mocked etudiantService
         Departement result = departementService.updateDepartement(department);
-        // Verify that the mocked service was called with the correct arguments
         verify(departementService, times(1)).updateDepartement(any(Departement.class));
-        // Check if the result matches the expected values
-        // Check if the result matches the expected values
         assertEquals("Computer Science" , result.getNomDepart());}
 
     @Test
     public void testRemoveDepartment() {
-        // Define the behavior of the mocked etudiantService
         doNothing().when(departementService).deleteDepartement(123);
-// Test the service that depends on the mocked etudiantService
         departementService.deleteDepartement(123);
-        // Verify that the mocked service was called with the correct arguments
         verify(departementService, times(1)).deleteDepartement(123);
     }}
